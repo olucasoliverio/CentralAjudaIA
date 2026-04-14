014d45e2c7d75548424b8bdf365ba2f7e157a533
Lucas <olucasoliverio@gmail.com>
2026-04-14 20:47:47 -0300

    a

--- central-ajuda-ui/src/components/Articles.tsx
+++ central-ajuda-ui/src/components/Articles.tsx
@@ -150,7 +150,7 @@ export function Articles() {
             gap: 8,
             margin: '0 auto 12px',
             alignItems: 'center',
-            width: '110%',
+            width: '100%',
             maxWidth: 1100,
           }}
         >
